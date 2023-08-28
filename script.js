c77ae4e470444b275752a6c85ebfcfa9cbb4ee7f
Devesh Shukla <deveshshukla1603@gmail.com>
2023-08-28 17:59:00 +0530

    Added Some Functionality

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+var tim = new Date();
 var t = document.querySelector('#time');
+setInterval(()=>{
+    const currTime = new Date().getTime();
+    var incr = Math.round((currTime - tim.getTime())/1000); 
+},1000)
 
+function startFunc(){
+    let sec = document.querySelector('#sec')
+    var i = 0;
+}
+
+function incre(i) {
+    setInterval(function(){
+        sec.textContent = i++;
+    },1000);
+}
