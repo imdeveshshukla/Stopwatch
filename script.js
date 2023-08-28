a6ad76eea93e946761e0074adfcf893bfc81d363
Devesh Shukla <deveshshukla1603@gmail.com>
2023-08-28 18:12:02 +0530

    Trying to make logic

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 var tim = new Date();
 var t = document.querySelector('#time');
-setInterval(()=>{
-    const currTime = new Date().getTime();
-    var incr = Math.round((currTime - tim.getTime())/1000); 
-},1000)
+var sec = document.querySelector('#sec')
 
 function startFunc(){
-    let sec = document.querySelector('#sec')
     var i = 0;
+    incre(i);
+}
+function stopFunc() {
+    
+}
+function incre(i) {
+    // let i = 0 ;
+    // setInterval(function(){
+    //     sec.textContent = i++;
+    // },1000);
+    if((sec.textContent)/1 < 10) sec.textContent = `0${i}`
+    sec.textContent = i;
+    i++;
 }
 
-function incre(i) {
-    setInterval(function(){
-        sec.textContent = i++;
-    },1000);
-}
+
+
+
+
+
+// setInterval(()=>{
+//     const currTime = new Date().getTime();
+//     var incr = Math.round((currTime - tim.getTime())/1000); 
+// },1000)
